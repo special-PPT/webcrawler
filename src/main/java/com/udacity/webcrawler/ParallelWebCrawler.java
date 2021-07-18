9d116c3913026894dc480a5772c0d6f6f4dbd091
special-PPT <xppt1993@gmail.com>
2021-07-18 15:31:12 +0800

    First commit

---
+++ src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
@@ -0,0 +1,120 @@
+package com.udacity.webcrawler;
+
+import com.udacity.webcrawler.json.CrawlResult;
+import com.udacity.webcrawler.parser.PageParser;
+import com.udacity.webcrawler.parser.PageParserFactory;
+
+import javax.inject.Inject;
+import javax.inject.Provider;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * A concrete implementation of {@link WebCrawler} that runs multiple threads on a
+ * {@link ForkJoinPool} to fetch and process multiple web pages in parallel.
+ */
+final class ParallelWebCrawler implements WebCrawler {
+  private final Clock clock;
+  private final Duration timeout;
+  private final int popularWordCount;
+  private final ForkJoinPool pool;
+  private final PageParserFactory parserFactory;
+  private final int maxDepth;
+  private final List<Pattern> ignoredUrls;
+
+  @Inject
+  ParallelWebCrawler(
+      Clock clock,
+      PageParserFactory parserFactory,
+      @Timeout Duration timeout,
+      @PopularWordCount int popularWordCount,
+      @TargetParallelism int threadCount,
+      @MaxDepth int maxDepth,
+      @IgnoredUrls List<Pattern> ignoredUrls) {
+    this.clock = clock;
+    this.timeout = timeout;
+    this.popularWordCount = popularWordCount;
+    this.pool = new ForkJoinPool(Math.min(threadCount, getMaxParallelism()));
+    this.parserFactory = parserFactory;
+    this.maxDepth = maxDepth;
+    this.ignoredUrls = ignoredUrls;
+  }
+
+  @Override
+  public CrawlResult crawl(List<String> startingUrls) {
+    Instant deadline = clock.instant().plus(timeout);
+    Map<String, Integer> counts = new ConcurrentHashMap<>();
+    Set<String> visitedUrls = ConcurrentHashMap.newKeySet();
+    for (String url : startingUrls) {
+      pool.invoke(new crawlInternal(url, deadline, maxDepth, counts, visitedUrls));
+    }
+
+    if (counts.isEmpty()) {
+      return new CrawlResult.Builder()
+              .setWordCounts(counts)
+              .setUrlsVisited(visitedUrls.size())
+              .build();
+    }
+    return new CrawlResult.Builder()
+            .setWordCounts(WordCounts.sort(counts, popularWordCount))
+            .setUrlsVisited(visitedUrls.size())
+            .build();
+  }
+
+  public final class crawlInternal extends RecursiveAction {
+    private final String url;
+    private final Instant deadline;
+    private final int maxDepth;
+    private final Map<String, Integer> counts;
+    Set<String> visitedUrls;
+
+    public crawlInternal(String url, Instant deadline, int maxDepth, Map<String, Integer> counts, Set<String> visitedUrls) {
+      this.url = url;
+      this.deadline = deadline;
+      this.maxDepth = maxDepth;
+      this.counts = counts;
+      this.visitedUrls = visitedUrls;
+    }
+    @Override
+    protected void compute() {
+      if (maxDepth == 0 || clock.instant().isAfter(deadline)) {
+        return;
+      }
+      for (Pattern pattern : ignoredUrls) {
+        if (pattern.matcher(url).matches()) {
+          return;
+        }
+      }
+      if (visitedUrls.contains(url)) {
+        return;
+      }
+      visitedUrls.add(url);
+      PageParser.Result result = parserFactory.get(url).parse();
+      for (Map.Entry<String, Integer> e : result.getWordCounts().entrySet()) {
+        if (counts.containsKey(e.getKey())) {
+          counts.put(e.getKey(), e.getValue() + counts.get(e.getKey()));
+        } else {
+          counts.put(e.getKey(), e.getValue());
+        }
+      }
+      List<crawlInternal> crawlInternalList = new ArrayList<>();
+      for (String link : result.getLinks()) {
+        crawlInternalList.add(new crawlInternal(link, deadline, maxDepth - 1, counts, visitedUrls));
+      }
+      invokeAll(crawlInternalList);
+    }
+  }
+
+  @Override
+  public int getMaxParallelism() {
+    return Runtime.getRuntime().availableProcessors();
+  }
+}
---
+++ src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -0,0 +1,56 @@
+package com.udacity.webcrawler.profiler;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Objects;
+
+/**
+ * A method interceptor that checks whether {@link Method}s are annotated with the {@link Profiled}
+ * annotation. If they are, the method interceptor records how long the method invocation took.
+ */
+final class ProfilingMethodInterceptor implements InvocationHandler {
+
+  private final Clock clock;
+  private final Object target;
+  private final ProfilingState profilingState;
+
+  // TODO: You will need to add more instance fields and constructor arguments to this class.
+  ProfilingMethodInterceptor(Clock clock, Object target, ProfilingState profilingState) {
+    this.clock = Objects.requireNonNull(clock);
+    this.target = target;
+    this.profilingState = profilingState;
+  }
+
+  public boolean isProfiled(Method method) {
+    return method.isAnnotationPresent(Profiled.class);
+  }
+  @Override
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    // TODO: This method interceptor should inspect the called method to see if it is a profiled
+    //       method. For profiled methods, the interceptor should record the start time, then
+    //       invoke the method using the object that is being profiled. Finally, for profiled
+    //       methods, the interceptor should record how long the method call took, using the
+    //       ProfilingState methods.
+    Object result;
+    Instant start = null;
+    boolean isMethodProfiled = isProfiled(method);
+    if(isMethodProfiled) {
+      start = clock.instant();
+    }
+    try {
+      result = method.invoke(target, args);
+    } catch (Throwable t) {
+      throw t.getCause();
+    } finally {
+      if(isMethodProfiled) {
+        Duration duration = Duration.between(start, clock.instant());
+        profilingState.record(target.getClass(), method, duration);
+      }
+    }
+
+    return result;
+  }
+}
